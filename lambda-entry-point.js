8d74d21c0874652620b9759bab33d772c09e2db1
Jakub Niewczas <niewczas.jakub@gmail.com>
2024-05-06 12:32:29 +0900

    add basic dynamodb-lambda configuration

---
+++ lambda-entry-point.js
@@ -0,0 +1,31 @@
+const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const { marshall } = require("@aws-sdk/util-dynamodb");
+
+const db = new DynamoDBClient({});
+
+exports.handler = async _event => {
+  const response = { statusCode: 200 };
+  try {
+    const itemParams = { ID: "test", data: "hello world" };
+    const putParams = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Item: marshall(itemParams),
+    };
+    const createResult = await db.send(new PutItemCommand(putParams));
+
+    response.body = JSON.stringify({
+      message: "Successfully created item!",
+      createResult,
+    });
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed to create post.",
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+
+  return response;
+};
